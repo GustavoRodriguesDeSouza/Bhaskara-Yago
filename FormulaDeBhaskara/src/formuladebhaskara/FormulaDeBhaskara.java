723dd73212871cfeabd82f14d913bebcb0ed285b
Gustavo <129074506+GustavoRodriguesDeSouza@users.noreply.github.com>
2023-04-11 11:13:30 -0300

    Add files via upload

---
+++ FormulaDeBhaskara/src/formuladebhaskara/FormulaDeBhaskara.java
@@ -0,0 +1,20 @@
+
+package formuladebhaskara;
+
+public class FormulaDeBhaskara {
+
+    public static void main(String[] args) {
+      
+       Yago b1 = new Yago();
+       b1.setA(7);
+       b1.setB(1);
+       b1.setC(-3);
+       b1.ValorDelta();
+       b1.Valorx1();
+       b1.Valorx2();
+       b1.Status();
+    
+
+    }
+    
+}
